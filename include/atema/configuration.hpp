3f70470b05ebf6fd1e7db68eea240e3f5f20efc9
JordiSubirana <jordi.subirana@yahoo.fr>
2016-04-21 19:34:52 +0200

    Update config, fix opengl source.

--- include/atema/configuration.hpp
+++ include/atema/configuration.hpp
@@ -4,6 +4,13 @@
 #ifdef _WIN32
 
 	#define ATEMA_SYSTEM_WINDOWS
+	
+	#define ATEMA_EXPORT __declspec(dllexport)
+	#define ATEMA_IMPORT __declspec(dllimport)
+	
+	#ifndef NOMINMAX
+		#define NOMINMAX
+	#endif
 
 #elif __APPLE__
 
@@ -11,17 +18,42 @@
 	
 	#if TARGET_OS_MAC
 		#define ATEMA_SYSTEM_MACOS
+		
+		#define ATEMA_EXPORT
+		#define ATEMA_IMPORT
+	#else
+		#define ATEMA_SYSTEM_UNKNOWN
+		
+		#define ATEMA_EXPORT __attribute__((visibility ("default")))
+		#define ATEMA_IMPORT __attribute__((visibility ("default")))
+		
+		#error Apple operating system not supported by ATEMA
 	#endif
 	
 #elif __unix__
 	
 	#if __linux__
 		#define ATEMA_SYSTEM_LINUX
+		
+		#define ATEMA_EXPORT __attribute__((visibility ("default")))
+		#define ATEMA_IMPORT __attribute__((visibility ("default")))
+	#else
+		#define ATEMA_SYSTEM_UNKNOWN
+		
+		#define ATEMA_EXPORT
+		#define ATEMA_IMPORT
+		
+		#error Unix operating system not supported by ATEMA
 	#endif
 	
 #else
 	
-	#error "Operating system unsupported by ATEMA"
+	#define ATEMA_SYSTEM_UNKNOWN
+	
+	#define ATEMA_EXPORT
+	#define ATEMA_IMPORT
+	
+	#error Operating system not supported by ATEMA
 
 #endif
 
--- include/atema/context/opengl.hpp
+++ include/atema/context/opengl.hpp
@@ -5,7 +5,7 @@
 
 // glBindBuffer
 #undef glBindBuffer
-ATEMA_API extern void at_glBindBuffer(GLenum target, GLuint buffer);
+GLAPI void at_glBindBuffer(GLenum target, GLuint buffer);
 #define glBindBuffer at_glBindBuffer
 
 #endif
