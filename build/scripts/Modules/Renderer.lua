db399adb39249650901ccff40274dd4ab94cd57e
Jordi <jordi.subirana@yahoo.fr>
2021-05-07 16:27:04 +0200

    Added premake scripts for project generation

---
+++ build/scripts/Modules/Renderer.lua
@@ -0,0 +1,12 @@
+-- Additionnal defines of the module
+MODULE.defines = {}
+
+-- Extlibs (third party embedded sources)
+MODULE.extlibs = {}
+
+-- Dependencies (Atema modules & compiled extlibs)
+MODULE.dependencies = {"AtemaCore"}
+
+-- OS specific dependencies (compiled)
+MODULE.osDependencies.windows = {}
+MODULE.osDependencies.posix = {}
