248d1e9fa4d5d145837029ab9261f89c8db64875
Jordi <jordi.subirana@yahoo.fr>
2021-05-07 16:49:37 +0200

    Added module specific config files

---
+++ include/Atema/Renderer/Config.hpp
@@ -0,0 +1,45 @@
+/*
+	Copyright 2021 Jordi SUBIRANA
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+	Software, and to permit persons to whom the Software is furnished to do so, subject
+	to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+	PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef ATEMA_RENDERER_CONFIG_HPP
+#define ATEMA_RENDERER_CONFIG_HPP
+
+#include <Atema/Config.hpp>
+
+#if defined(ATEMA_STATIC)
+	
+	#define ATEMA_RENDERER_API
+	
+#else
+	
+	#if defined(ATEMA_RENDERER_EXPORT)
+		
+		#define ATEMA_RENDERER_API ATEMA_EXPORT
+		
+	#else
+		
+		#define ATEMA_RENDERER_API ATEMA_IMPORT
+		
+	#endif
+
+#endif
+
+#endif
