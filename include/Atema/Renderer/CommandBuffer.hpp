134da38ab0fa12a511f8fd6fd3aea74db94dfd28
Jordi <jordi.subirana@yahoo.fr>
2021-08-12 14:38:37 +0200

    Renderer - CommandBuffer - Added vertex/index buffer copy, bindings, draw commands

--- include/Atema/Renderer/CommandBuffer.hpp
+++ include/Atema/Renderer/CommandBuffer.hpp
@@ -25,12 +25,13 @@
 #include <Atema/Renderer/Config.hpp>
 #include <Atema/Core/NonCopyable.hpp>
 #include <Atema/Core/Pointer.hpp>
-#include <Atema/Core/Vector.hpp>
+#include <Atema/Renderer/Enums.hpp>
 
 #include <vector>
 
 namespace at
 {
+	class Buffer;
 	class CommandPool;
 	class RenderPass;
 	class Framebuffer;
@@ -42,6 +43,7 @@ namespace at
 		struct Settings
 		{
 			Ptr<CommandPool> commandPool;
+			bool singleUse = false;
 		};
 		
 		typedef union ClearValue
@@ -68,6 +70,16 @@ namespace at
 
 		virtual void endRenderPass() = 0;
 
+		virtual void copyBuffer(const Ptr<Buffer>& srcBuffer, const Ptr<Buffer>& dstBuffer, size_t size, size_t srcOffset = 0, size_t dstOffset = 0) = 0;
+
+		virtual void bindVertexBuffer(const Ptr<Buffer>& buffer, uint32_t binding) = 0;
+
+		virtual void bindIndexBuffer(const Ptr<Buffer>& buffer, IndexType indexType) = 0;
+
+		virtual void draw(uint32_t vertexCount, uint32_t instanceCount = 1, uint32_t firstVertex = 0, uint32_t firstInstance = 0) = 0;
+
+		virtual void drawIndexed(uint32_t indexCount, uint32_t instanceCount = 1, uint32_t firstIndex = 0, int32_t vertexOffset = 0, uint32_t firstInstance = 0) = 0;
+		
 		virtual void end() = 0;
 		
 	protected:
