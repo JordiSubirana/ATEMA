06144f981fabf3415d1a50ebfc3909b0612043e2
jordi <jordi.subirana@yahoo.fr>
2022-02-09 14:21:11 +0100

    Shader module

--- include/Atema/Shader.hpp
+++ include/Atema/Shader.hpp
@@ -19,16 +19,9 @@
 	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#ifndef ATEMA_GLOBAL_ATEMA_HPP
-#define ATEMA_GLOBAL_ATEMA_HPP
+#ifndef ATEMA_GLOBAL_SHADER_HPP
+#define ATEMA_GLOBAL_SHADER_HPP
 
-#include <Atema/Config.hpp>
-#include <Atema/Core.hpp>
-#include <Atema/Graphics.hpp>
-#include <Atema/Math.hpp>
-#include <Atema/Renderer.hpp>
-#include <Atema/Shader.hpp>
-#include <Atema/VulkanRenderer.hpp>
-#include <Atema/Window.hpp>
+#include <Atema/Shader/Config.hpp>
 
 #endif
