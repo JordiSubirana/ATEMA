a606b7efd9a5e91e0f0d139ae2982c76c21b8cb9
jordi <jordi.subirana@yahoo.fr>
2022-02-09 14:28:28 +0100

    Shader - ATSL - Tokens & utility functions

---
+++ include/Atema/Shader/Atsl/AtslToken.hpp
@@ -0,0 +1,127 @@
+/*
+	Copyright 2021 Jordi SUBIRANA
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+	Software, and to permit persons to whom the Software is furnished to do so, subject
+	to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+	PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef ATEMA_SHADER_ATSLTOKEN_HPP
+#define ATEMA_SHADER_ATSLTOKEN_HPP
+
+#include <Atema/Shader/Config.hpp>
+#include <Atema/Core/Variant.hpp>
+
+#include <string>
+
+namespace at
+{
+	enum class AtslTokenType
+	{
+		Symbol,
+		Keyword,
+		Identifier,
+		Value,
+	};
+
+	enum class AtslSymbol
+	{
+		Plus,
+		Minus,
+		Multiply,
+		Divide,
+		Power,
+		Modulo,
+
+		Equal,
+		Less,
+		Greater,
+
+		And,
+		Or,
+		Not,
+
+		Dot,
+		Comma,
+		Colon,
+		Semicolon,
+
+		LeftBrace,
+		RightBrace,
+		LeftBracket,
+		RightBracket,
+		LeftParenthesis,
+		RightParenthesis,
+	};
+
+	enum class AtslKeyword
+	{
+		Input,
+		Output,
+		External,
+		Option,
+
+		Const,
+
+		If,
+		Else,
+
+		Struct,
+
+		Return,
+	};
+
+	using AtslIdentifier = std::string;
+
+	using AtslBasicValue = Variant<
+		bool,
+		uint32_t,
+		float>;
+
+	using AtslTokenValue = Variant<
+		AtslSymbol,
+		AtslKeyword,
+		AtslIdentifier,
+		AtslBasicValue>;
+
+	struct ATEMA_SHADER_API AtslToken
+	{
+		AtslToken() = delete;
+		AtslToken(AtslSymbol symbol);
+		AtslToken(AtslKeyword keyword);
+		AtslToken(const AtslIdentifier& identifier);
+		AtslToken(bool value);
+		AtslToken(uint32_t value);
+		AtslToken(float value);
+
+		bool is(AtslSymbol symbol) const noexcept;
+		bool is(AtslKeyword keyword) const noexcept;
+		bool is(const AtslIdentifier& identifier) const noexcept;
+		bool is(bool value) const noexcept;
+		bool is(uint32_t value) const noexcept;
+		bool is(float value) const noexcept;
+		
+		AtslTokenType type;
+		AtslTokenValue value;
+
+		size_t line;
+		size_t column;
+	};
+
+	
+}
+
+#endif
